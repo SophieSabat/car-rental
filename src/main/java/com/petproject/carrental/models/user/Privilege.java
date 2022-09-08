564c879ce8335236f2250c6e3437f89bffc380ef
[RZfdMcEh] <[sonyasab2000@gmail.com]>
2022-09-08 20:24:48 +0300

    Init Commit

--- src/main/java/com/petproject/carrental/models/user/Privilege.java
+++ src/main/java/com/petproject/carrental/models/user/Privilege.java
@@ -7,7 +7,6 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Collection;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
--- src/main/java/com/petproject/carrental/models/user/User.java
+++ src/main/java/com/petproject/carrental/models/user/User.java
@@ -28,7 +28,7 @@ public class User {
     @Column(name = "is_enable")
     private boolean isEnable;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
