93ba52c7048d92c0f0648590e61a1d35d67c1871
[RZfdMcEh] <[sonyasab2000@gmail.com]>
2023-05-09 00:04:52 +0300

    Init Commit

--- src/main/java/com/petproject/carrental/repository/CarRepository.java
+++ src/main/java/com/petproject/carrental/repository/CarRepository.java
@@ -2,9 +2,11 @@ package com.petproject.carrental.repository;
 
 import com.petproject.carrental.model.car.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
     @Override
